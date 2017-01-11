e09d74cd4fdd8d717c3c701bbb35c951533a814b
reihsr <robert.reihs@gmail.com>
2017-01-11 18:02:36 +0100

    Reset the setup script

--- app-install/install.py
+++ app-install/install.py
@@ -10,7 +10,19 @@ def updatePorts(template, portmap):
     for portname in portmap.keys():
         if(portname.startswith("§§")):
             template = template.replace(portname, portmap[portname])
+        elif(portname == "instance"):
+            template = template.replace("§§INSTANCE", portmap[portname])
+    return template
 
+def updateParameters(template, environment):
+    for var in environment.keys():
+        template = template.replace(var, environment[var])
+    return template
+
+def updateEnvironment(template, environment):
+    for var in environment.keys():
+        template = template.replace(var, environment[var])
+    return template
 
 print ("SETUP UP DOCKER-COMPOSE")
 
@@ -29,12 +41,15 @@ template = open(instancepath + '/docker-compose-template.yml', 'r').read()
 
 with open(instancepath + "/portmap.json") as data_file:
     portmap = json.load(data_file)
+template = updatePorts(template, portmap)
 
 with open(instancepath + "/environment-parameters-settings.json") as data_file:
     environment = json.load(data_file)
+template = updateParameters(template, environment)
 
-with open(instancepath + "/config-parameters.json") as data_file:
+with open(instancepath + "/config-parameters-settings.json") as data_file:
     config = json.load(data_file)
+template = updateParameters(template, environment)
 
 target = open(instancepath + "/docker-compose.yml", 'w')
 target.write(template)
--- setup-liferay/scripts/main.py
+++ setup-liferay/scripts/main.py
@@ -14,49 +14,44 @@ import users
 
 print ("SETUP SCRIPT FOT eB3KIT BIBBOX DEMO")
 
-template = "SETUP SCRIPT FOT eB3KIT BIBBOX DEMO"
-target = open("/opt/bibbox/test.log", 'w')
-target.write(template)
-target.close()
+def testServerStarted(counter):
+    try:
+        auth = ('test@liferay.com', 'test')
+        url = "http://localhost:8080/api/jsonws/BIBBOXDocker-portlet.get-updated-application-store-list"
+        headers = {
+            'content-type': "application/json",
+            'cache-control': "no-cache"
+        }
 
-# def testServerStarted(counter):
-#     try:
-#         auth = ('test@liferay.com', 'test')
-#         url = "http://localhost:8080/api/jsonws/BIBBOXDocker-portlet.get-updated-application-store-list"
-#         headers = {
-#             'content-type': "application/json",
-#             'cache-control': "no-cache"
-#         }
-#
-#         r = requests.get(url, auth=auth, headers=headers)
-#     except requests.exceptions.Timeout:
-#         # Maybe set up for a retry, or continue in a retry loop
-#         print("Connection try:" + counter + "| Server still starting up, connection Timed out.")
-#         if(counter > 10):
-#             return
-#         time.sleep(15)
-#         testServerStarted(counter + 1)
-#     except requests.exceptions.TooManyRedirects:
-#         print("Error to many Redirects")
-#         # Tell the user their URL was bad and try a different one
-#     except requests.exceptions.RequestException as e:
-#         # catastrophic error. bail.
-#         print(e)
-#         print("Connection try:" + counter + "| Error connecting to API.")
-#         if (counter > 10):
-#             return
-#         time.sleep(15)
-#         testServerStarted(counter + 1)
-#
-# print("Trying to connect to liferay server.")
-# testServerStarted(0)
-#
-# print ("SETUP SITES")
-# siteService = sites.Sites(companyId = '20116')
-# siteService.initSites()
-#
-# print ("SETUP USERS")
-# userService = users.Users (companyId = '20116')
-# userService.initUsers()
-#
-# api = jsonws.API()
+        r = requests.get(url, auth=auth, headers=headers)
+    except requests.exceptions.Timeout:
+        # Maybe set up for a retry, or continue in a retry loop
+        print("Connection try:" + counter + "| Server still starting up, connection Timed out.")
+        if(counter > 10):
+            return
+        time.sleep(15)
+        testServerStarted(counter + 1)
+    except requests.exceptions.TooManyRedirects:
+        print("Error to many Redirects")
+        # Tell the user their URL was bad and try a different one
+    except requests.exceptions.RequestException as e:
+        # catastrophic error. bail.
+        print(e)
+        print("Connection try:" + counter + "| Error connecting to API.")
+        if (counter > 10):
+            return
+        time.sleep(15)
+        testServerStarted(counter + 1)
+
+print("Trying to connect to liferay server.")
+testServerStarted(0)
+
+print ("SETUP SITES")
+siteService = sites.Sites(companyId = '20116')
+siteService.initSites()
+
+print ("SETUP USERS")
+userService = users.Users (companyId = '20116')
+userService.initUsers()
+
+api = jsonws.API()
