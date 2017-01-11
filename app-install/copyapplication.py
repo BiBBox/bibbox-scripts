a23ba96c6d739f7e197580f77f35e948a6843d64
reihsr <robert.reihs@gmail.com>
2017-01-11 10:40:20 +0100

    Update setup and install scripts
    Timeout script added to setup script
    Copy script for installing

--- app-install/copyapplication.py
+++ app-install/copyapplication.py
@@ -1,2 +1,54 @@
 #!/usr/bin/env python
 
+import os
+import sys
+import getopt
+import shutil
+import json
+
+from pprint import pprint
+
+def createFolders(instancepath, folder):
+    directory = instancepath + "/" + folder
+    print("Creating folders: " + directory)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def copyFiles(applicationpath, source, instancepath, destination):
+    try:
+        print("Copy file: " + destination)
+        src = applicationpath + "/" + source
+        dest = instancepath + "/" + destination
+        shutil.copy2(src, dest)
+    except:
+        print("Unexpected copy error:" + str(sys.exc_info()[1]))
+
+argv = sys.argv[1:]
+opts, args = getopt.getopt(argv,"a:i:",["applicationpath=","instancepath="])
+for opt, arg in opts:
+    if opt == '-h':
+        print('test.py -a <applicationpath> -i <instancepath>')
+        sys.exit()
+    elif opt in ("-a", "--applicationpath"):
+        applicationpath = arg
+    elif opt in ("-i", "--instancepath"):
+        instancepath = arg
+
+dir = os.path.dirname(os.path.realpath(__file__))
+applicationfiles = os.path.join(dir, 'config/applicationfiles.json')
+
+with open(applicationfiles) as data_file:
+    applicationfiles_json = json.load(data_file)
+
+with open(applicationpath + '/file_structure.json') as data_file:
+    file_structure_json = json.load(data_file)
+
+print("----------------")
+for folder in file_structure_json["makefolders"]:
+    createFolders(instancepath, folder)
+print("----------------")
+for folder in file_structure_json["copyfiles"]:
+    copyFiles(applicationpath, folder['source'], instancepath, folder['destination'])
+print("----------------")
+for folder in applicationfiles_json["copyapplicationfiles"]:
+    copyFiles(applicationpath, folder['source'], instancepath, folder['destination'])
--- setup-liferay/scripts/main.py
+++ setup-liferay/scripts/main.py
@@ -12,6 +12,34 @@ import users
 
 print ("SETUP SCRIPT FOT eB3KIT BIBBOX DEMO")
 
+def testServerStarted(counter):
+    try:
+        auth = ('test@liferay.com', 'test')
+        url = "http://localhost:8080/api/jsonws/BIBBOXDocker-portlet.get-updated-application-store-list"
+        headers = {
+            'content-type': "application/json",
+            'cache-control': "no-cache"
+        }
+        r = requests.get(url, auth=auth, headers=headers)
+    except requests.exceptions.Timeout:
+        # Maybe set up for a retry, or continue in a retry loop
+        print("Connection try:" + counter + "| Server still starting up, connection Timed out.")
+        if(counter > 10):
+            return
+        testServerStarted(counter + 1)
+    except requests.exceptions.TooManyRedirects:
+        print("Error to many Redirects")
+        # Tell the user their URL was bad and try a different one
+    except requests.exceptions.RequestException as e:
+        # catastrophic error. bail.
+        print(e)
+        if (counter > 10):
+            return
+        testServerStarted(counter + 1)
+
+
+testServerStarted(0)
+
 print ("SETUP SITES")
 siteService = sites.Sites(companyId = '20116')
 siteService.initSites()
