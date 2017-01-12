75714258c92bb174462397e8761ca0561bbd6a7c
reihsr <robert.reihs@gmail.com>
2017-01-12 11:35:30 +0100

    Merger file

--- setup-liferay/scripts/main.py
+++ setup-liferay/scripts/main.py
@@ -3,13 +3,9 @@
 import json
 import urllib
 import time
-<<<<<<< HEAD
 import os
 import sys
 import errno
-=======
-import sys
->>>>>>> origin/master
 
 import requests
 import jsonws
@@ -39,34 +35,19 @@ def testServerStarted(counter):
             time.sleep(15)
             testServerStarted(counter + 1)
     except requests.exceptions.Timeout:
-        # Maybe set up for a retry, or continue in a retry loop
-<<<<<<< HEAD
         print("Connection try:" + str(counter) + "| Server still starting up, connection Timed out.")
         if(counter > 20):
             sys.exit(errno.ETIME)
-=======
-        sys.stderr.write("Connection try:" + counter + "| Server still starting up, connection Timed out.")
-        if(counter > 10):
-            return
->>>>>>> origin/master
         time.sleep(15)
         testServerStarted(counter + 1)
     except requests.exceptions.TooManyRedirects:
         sys.stderr.write("Error to many Redirects")
         # Tell the user their URL was bad and try a different one
     except requests.exceptions.RequestException as e:
-        # catastrophic error. bail.
-<<<<<<< HEAD
         print(e)
         print("Connection try:" + str(counter) + "| Error connecting to API.")
         if (counter > 20):
             sys.exit(errno.ETIME)
-=======
-        sys.stderr.write(e)
-        sys.stderr.write("Connection try:" + counter + "| Error connecting to API.")
-        if (counter > 10):
-            return
->>>>>>> origin/master
         time.sleep(15)
         testServerStarted(counter + 1)
 
