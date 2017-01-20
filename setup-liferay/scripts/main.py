2cf2a89a5b286a063ff0e7f21680895c987f7f11
reihsr <robert.reihs@gmail.com>
2017-01-20 11:10:23 +0100

    Add new logging

--- setup-liferay/scripts/main.py
+++ setup-liferay/scripts/main.py
@@ -6,6 +6,7 @@ import time
 import os
 import sys
 import errno
+import logging
 
 import requests
 import jsonws
@@ -16,6 +17,7 @@ import roles
 import users
 
 sys.stdout.write("SETUP SCRIPT FOR eB3KIT BIBBOX DEMO \n")
+logger = logging.getLogger("app-analyser")
 
 def testServerStarted(counter):
     try:
@@ -30,35 +32,53 @@ def testServerStarted(counter):
 
         if(r.status_code != requests.codes.ok):
             print("Connection try:" + str(counter) + "| Bad API Response. " + str(r.status_code))
+            logger.info("Connection try:" + str(counter) + "| Bad API Response. " + str(r.status_code))
             if (counter > 60):
                 sys.exit(errno.ETIME)
             time.sleep(30)
             testServerStarted(counter + 1)
     except requests.exceptions.Timeout:
         print("Connection try:" + str(counter) + "| Server still starting up, connection Timed out.")
+        logger.info("Connection try:" + str(counter) + "| Server still starting up, connection Timed out.")
         if(counter > 60):
             sys.exit(errno.ETIME)
         time.sleep(30)
         testServerStarted(counter + 1)
     except requests.exceptions.TooManyRedirects:
         sys.stderr.write("Error to many Redirects")
+        logger.info("Error to many Redirects")
         # Tell the user their URL was bad and try a different one
     except requests.exceptions.RequestException as e:
         print(e)
         print("Connection try:" + str(counter) + "| Error connecting to API.")
+        logger.info("Connection try:" + str(counter) + "| Error connecting to API.")
         if (counter > 60):
             sys.exit(errno.ETIME)
         time.sleep(30)
         testServerStarted(counter + 1)
 
+
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+fh = logging.FileHandler('/opt/bibbox/setup-liferay.log')
+fh.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+# add the handlers to logger
+logger.addHandler(fh)
+
 sys.stdout.write("Trying to connect to liferay server. \n")
+logger.info("Trying to connect to liferay server. \n")
 testServerStarted(0)
 
 sys.stdout.write("SETUP SITES \n")
+logger.info("SETUP SITES \n")
 siteService = sites.Sites(companyId = '20116')
 siteService.initSites()
 
 sys.stdout.write("SETUP USERS \n")
+logger.info("SETUP USERS \n")
 userService = users.Users (companyId = '20116')
 userService.initUsers()
 
