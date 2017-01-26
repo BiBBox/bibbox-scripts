a5267c8ce48120ad03993a0ad91ffbd383f5075f
reihsr <robert.reihs@gmail.com>
2017-01-26 11:23:09 +0100

    Update start Script with attache and log file for setup

--- initscripts/start.sh
+++ initscripts/start.sh
@@ -4,6 +4,6 @@ echo "Start BIBBOX-VM Services"
 source /etc/bibbox/bibbox.cfg
 
 # Start Folder listener to restart apache2 when new config added
-. "$bibboxdir/$bibboxscriptfolder/initscripts/apacheServiceListener.sh" > /var/log/apacheServiceListener.log 2>&1 &
+. "$bibboxdir/$bibboxscriptfolder/initscripts/apacheServiceListener.sh" >> /var/log/apacheServiceListener.log 2>&1 &
 
-. "$bibboxdir/$bibboxscriptfolder/initscripts/runSetupScript.sh" &
+. "$bibboxdir/$bibboxscriptfolder/initscripts/runSetupScript.sh" >> /var/log/liferaySetup.log 2>&1 &
