a9dbb199b0de75f67c519edbbce46f4f83a51b34
lomadi <lomadi@mac.com>
2017-02-19 16:07:11 +0100

    remove debug output

--- app-install/install.py
+++ app-install/install.py
@@ -69,18 +69,18 @@ with open(applicationpath + '/file_structure.json') as data_file:
     file_structure_json = json.load(data_file)
 
 for filename in file_structure_json["configs-to-adapt"]:
-    print("REPLACE ENVIRONMENT IN", filename)
+    print("REPLACE ENVIRONMENT in ", filename)
 #    os.system("ls -a " + instancepath + '/'+ filename) 
 #    os.system("more " + instancepath + '/'+ filename) 
     source = open(instancepath + '/'+ filename, 'r');
     origdata = source.read()
-    print("source = ", origdata)
-    
+#    print("source = ", origdata)
     source.close()
+ 
     replaced_data = updateParameters(origdata,      environment)
     replaced_data = updateParameters(replaced_data, config)
     
-    print("replaced = ", replaced_data )
+#    print("replaced = ", replaced_data )
     target = open(instancepath + '/'+ filename + ".new", 'w');
     target.write(replaced_data)
     target.close()
