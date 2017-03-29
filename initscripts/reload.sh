26967dd7f6cc228cf09ed27f5df5f5259235d752
reihsr <robert.reihs@gmail.com>
2017-03-29 16:09:21 +0200

    Update reload.sh

--- initscripts/reload.sh
+++ initscripts/reload.sh
@@ -2,5 +2,7 @@
 
 echo "reload.sh"
 
-# ps aux | grep "apacheServiceListener.sh"
+# ps aux | grep "sites-enabled"
+#root        1222  0.0  0.0   6508   900 ?        S    Mar23   0:00 inotifywait -m /etc/apache2/sites-enabled -e delete -e create -e moved_to
+#root        8576  0.0  0.0   6508    84 ?        S    Mar23   0:00 inotifywait -m /etc/apache2/sites-enabled -e delete -e create -e moved_to
 # Stop watcher, start watcher
