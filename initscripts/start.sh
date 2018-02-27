6e2c37e0e1bd018769780648685f7dbd2f7e5518
reihsr <robert.reihs@gmail.com>
2018-02-27 13:49:53 +0100

    Update start.sh

--- initscripts/start.sh
+++ initscripts/start.sh
@@ -5,5 +5,3 @@ source /etc/bibbox/bibbox.cfg
 
 # Start Folder listener to restart apache2 when new config added
 . "$bibboxdir/$bibboxscriptfolder/initscripts/apacheServiceListener.sh" >> /var/log/apacheServiceListener.log 2>&1 &
-
-. "$bibboxdir/$bibboxscriptfolder/initscripts/runSetupScript.sh" >> /var/log/liferaySetup.log 2>&1 &
